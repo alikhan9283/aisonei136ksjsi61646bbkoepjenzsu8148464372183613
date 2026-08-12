f3baa131962c073f34c20ab41188e4ea88e39c2d
alikhan9283 <didixii@outlook.com>
2026-08-12 16:21:16 +0500

    Create sar-img.js

---
+++ plugins/sar-img.js
@@ -0,0 +1,69 @@
+// SARWAR-MD
+const axios = require('axios');
+const config = require('../config');
+const { cmd } = require('../command');
+
+function getPinterestSearch(query) {
+    const apis = [
+        `https://api.nexray.eu.cc/search/pinterest?q=${encodeURIComponent(query)}`
+    ]
+    
+    const getAll = async () => {
+        for (const url of apis) {
+            try {
+                const res = await axios.get(url)
+                const data = res.data
+                
+                let urls = []
+                if (Array.isArray(data?.result)) {
+                    urls = data.result
+                        .map(item => item.images_url)
+                        .filter(u => typeof u === 'string' && u.startsWith('http'))
+                }
+                
+                if (urls.length) return urls
+            } catch {}
+        }
+        return []
+    }
+    
+    return { 
+        getAll,
+        getRandom: async () => {
+            const all = await getAll()
+            return all[Math.floor(Math.random() * all.length)] || null
+        }
+    }
+}
+
+cmd({
+    pattern: "img",
+    alias: ["img2", "image"],
+    react: "📌",
+    desc: "Search images on Google",
+    category: "search",
+    use: ".pinterest <query>",
+    filename: __filename
+}, async (conn, mek, m, { from, q, reply }) => {
+    try {
+        if (!q) return reply(`❀ Please enter a search query.\n\n*Example:* .img cat`)
+
+        const res = await getPinterestSearch(q)
+        const urls = await res.getAll()
+        
+        if (urls.length < 1) return reply('✧ No images found.')
+        
+        const medias = urls.slice(0, 10).map(url => ({ image: { url } }))
+        const caption = `> SARWAR-MD`
+        
+        for (let media of medias) {
+            await conn.sendMessage(from, media, { quoted: m })
+        }
+        
+        await conn.sendMessage(from, { text: caption }, { quoted: m })
+
+    } catch (error) {
+        console.error('Pinterest Search Error:', error)
+        reply(`⚠️ A problem has occurred.\n\n${error.message}`)
+    }
+})
